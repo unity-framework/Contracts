febc820e76a84cc9828249d0bff37f95ae7a0fca
e200 <eleandro@inbox.ru>
2017-10-23 21:29:35 +0100

    Added annotations and autowiring boolean methods

--- src/Container/Dependency/IDependencyFactory.php
+++ src/Container/Dependency/IDependencyFactory.php
@@ -27,4 +27,43 @@ interface IDependencyFactory
      * @return mixed|object
      */
     public function make($class, $dependencies = []);
+
+
+
+    /**
+     * Enable|Disable autowiring.
+     *
+     * Tells the container if it should auto wiring.
+     *
+     * @param bool $enable
+     *
+     * @return $this
+     */
+    public function enableAutowiring($enable);
+
+    /**
+     * Checks if injection is enabled.
+     *
+     * @return bool
+     */
+    protected function canAutowiring();
+
+    /**
+     * Enable|Disable the use of annotations.
+     *
+     * Tells the container if it can inspect annotations
+     * searching for properties or constructor dependencies.
+     *
+     * @param bool $enable
+     *
+     * @return $this
+     */
+    public function enableUseAnnotation($enable);
+
+    /**
+     * Checks if the use of annotations is enabled.
+     *
+     * @return bool
+     */
+    public function canUseAnnotations();
 }
